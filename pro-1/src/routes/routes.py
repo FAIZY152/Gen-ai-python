9385520cc99a00954f9bd38694623ac6bd0fe874
FAIZY152 <fayazjani7576@gmail.com>
2025-09-15 15:17:49 +0500

    MVC-pattern-applied

--- pro-1/src/routes/routes.py
+++ pro-1/src/routes/routes.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter, HTTPException
-from src.models import models
-
+from src.models import Agent
+from src.controller import AgentController
 
 router = APIRouter()
 
 @router.post("/")
-def create_agent(agent: models.Agent):
-    return agent_controller.create_agent_controller(agent)
+def create_agent(agent: Agent):
+    return AgentController.create_agent_controller(agent)
 
 @router.get("/")
 def list_agents():
-    return agent_controller.list_agents_controller()
+    
+    return AgentController.list_agents_controller()
 
 @router.get("/{agent_id}")
 def get_agent(agent_id: int):
-    return agent_controller.get_agent_controller(agent_id)
+    return AgentController.get_agent_controller(agent_id)
---
+++ pro-1/src/service/agent_service.py
@@ -0,0 +1,16 @@
+from src.models.Agent import Agent
+
+# Fake in-memory DB
+agents_db: list[Agent] = []
+
+def create_agent(agent: Agent):
+    agents_db.append(agent)
+    return {"message": f"Agent {agent.name} created successfully"}
+
+def list_agents():
+    return agents_db
+
+def get_agent(agent_id: int):
+    if 0 <= agent_id < len(agents_db):
+        return agents_db[agent_id]
+    return None
